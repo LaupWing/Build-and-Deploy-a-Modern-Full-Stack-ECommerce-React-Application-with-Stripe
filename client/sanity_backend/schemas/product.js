575087441e17f905bb97e1a6721c7c98a1600200
laupwing <loc_strict@hotmail.com>
2022-10-11 05:54:12 +0200

    name field

--- client/sanity_backend/schemas/product.js
+++ client/sanity_backend/schemas/product.js
@@ -11,6 +11,11 @@ export default {
          options:{
             hotspot: true
          }
+      },
+      {
+         name: "name",
+         title: "Name",
+         type: "string"
       }
    ]
 }
