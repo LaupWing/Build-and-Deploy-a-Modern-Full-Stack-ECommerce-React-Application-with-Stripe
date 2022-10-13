0adc756cb97a64c78ffd060170f87ff791650daf
laupwing <loc_strict@hotmail.com>
2022-10-13 05:36:15 +0200

    product

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,6 +7,10 @@ const Home = ()=>{
             <h2>Best selling products</h2>
             <p>Speakers of many variations</p>
          </div>
+
+         <div>
+            {["Product 1", "Product 2"].map(product=>product)}
+         </div>
       </>
    )
 }
