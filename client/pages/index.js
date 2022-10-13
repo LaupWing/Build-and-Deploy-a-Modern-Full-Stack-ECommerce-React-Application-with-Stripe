f78275cae644ed58ff4e1cd9b1350a43883ac278
laupwing <loc_strict@hotmail.com>
2022-10-13 07:36:50 +0200

    update

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,12 +3,12 @@ const Home = ()=>{
    return (
       <>
          Hero Banner 
-         <div>
+         <div className="products-heading">
             <h2>Best selling products</h2>
             <p>Speakers of many variations</p>
          </div>
 
-         <div>
+         <div className="products-container">
             {["Product 1", "Product 2"].map(product=>product)}
          </div>
       </>
