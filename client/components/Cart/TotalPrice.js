d9a706dd0ba673ae678d66cbae72875079004f0d
laupwing <loc_strict@hotmail.com>
2022-10-15 10:55:39 +0200

    total price

---
+++ client/components/Cart/TotalPrice.js
@@ -0,0 +1,21 @@
+const TotalPrice = ({totalPrice, handleCheckout}) => {
+   return (
+      <div className="cart-bottom">
+         <div className="total">
+            <h3>Subtotal:</h3>
+            <h3>${totalPrice}</h3>
+         </div>
+         <div className="btn-container">
+            <button
+               type="button"
+               className="btn"
+               onClick={handleCheckout}
+            >
+               pay with stripe
+            </button>
+         </div>
+      </div>
+   )
+}
+
+export default TotalPrice;
