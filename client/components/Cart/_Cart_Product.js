69bd5f348a9be4f7efd95623c0ef25f40d917d9c
laupwing <loc_strict@hotmail.com>
2022-10-15 11:02:44 +0200

    update

---
+++ client/components/Cart/_Cart_Product.js
@@ -0,0 +1,9 @@
+const CartProduct = () => {
+   return (  
+      <div className="product">
+         <img src="" alt="" className="cart-product-image" />
+      </div>
+   )
+}
+ 
+export default CartProduct;
