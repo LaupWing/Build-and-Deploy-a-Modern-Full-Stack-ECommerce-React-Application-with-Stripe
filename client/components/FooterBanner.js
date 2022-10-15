1497a270e70bfae1779619d79d8eb05fc4ba0963
laupwing <loc_strict@hotmail.com>
2022-10-15 10:15:28 +0200

    got all the fking paramters for the footerbanne rufnction

--- client/components/FooterBanner.js
+++ client/components/FooterBanner.js
@@ -1,6 +1,17 @@
-const FooterBanner = () =>{
+const FooterBanner = ({footerBanner:{
+   discount,
+   largeText1,
+   largeText2,
+   saleTime,
+   smallText,
+   midText,
+   desc,
+   product,
+   buttonText,
+   image
+}}) =>{
    return (
-      <div>
+      <div className="footer-banner-container">
          Footer
       </div>
    )
