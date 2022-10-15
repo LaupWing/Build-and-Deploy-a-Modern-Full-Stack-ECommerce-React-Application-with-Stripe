aa230cb15417485af3613beb98c7de9b8988ac6c
laupwing <loc_strict@hotmail.com>
2022-10-15 09:59:05 +0200

    Base navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,7 +1,23 @@
+import Link from "next/link"
+import {AiOutlineShopping} from "react-icons/ai"
+
 const Navbar = () =>{
    return (
-      <div>
-         Navbar
+      <div className="nav-container">
+         <p className="logo">
+            <Link href={"/"}>JSM Headphones</Link>
+         </p>
+         <button
+            type="button"
+            className="cart-icon"
+            onClick={()=>{}}
+         >
+            <AiOutlineShopping/>
+            <span className="cart-item-qty">
+               {/* quantities */}
+               100
+            </span>
+         </button>
       </div>
    )
 }
