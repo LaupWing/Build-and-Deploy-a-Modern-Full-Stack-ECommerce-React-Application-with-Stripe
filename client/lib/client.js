4b17d1076a38bb6bed9bd4bdfc56920fd4484f16
laupwing <loc_strict@hotmail.com>
2022-10-16 11:19:10 +0200

    easy

--- client/lib/client.js
+++ client/lib/client.js
@@ -1,1 +1,10 @@
 import sanityClient from "@sanity/client"
+import imageBuilder from "@sanity/image-url"
+
+const client = sanityClient({
+   projectId: "",
+   dataset: "",
+   apiVersion: "",
+   useCdn: true,
+   token: ""
+})
