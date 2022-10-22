74750fa0127d4c780a62e6b79fc016ccd5672764
LaupWing <loc_strict@hotmail.com>
2022-10-22 20:03:47 +0200

    update

--- sanity_backend/schemas/schema.js
+++ sanity_backend/schemas/schema.js
@@ -5,9 +5,9 @@ import product from "./product"
 import banner from "./banner"
 
 export default createSchema({
-  name: "default",
-  types: schemaTypes.concat([
-    product,
-    banner
-  ])
+   name: "default",
+   types: schemaTypes.concat([
+      product,
+      banner
+   ])
 })
