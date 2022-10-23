e2cf60d0c50e761053d97854175532cb98c7db60
LaupWing <loc_strict@hotmail.com>
2022-10-23 10:27:52 +0200

    get serveride props

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -18,4 +18,19 @@ const Product = () => {
    )
 }
 
+export const getServerSideProps = async ()=>{
+   const query = "*[_type == 'product']"
+   const products = await client.fetch(query)
+
+   const bannerQuery = "*[_type == 'banner']"
+   const banner = await client.fetch(bannerQuery)
+
+   return {
+      props: {
+         banner,
+         products
+      }
+   }
+}
+
 export default Product
