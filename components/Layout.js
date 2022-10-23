38f66975e9f42a7920f4522d176447d39edf690e
LaupWing <loc_strict@hotmail.com>
2022-10-23 10:16:28 +0200

    layout cmp

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,22 @@
+import Head from "next/head"
+import Footer from "./Footer"
+import Navbar from "./Navbar"
+
 const Layout = () =>{
    return (
-      <div>
-         Layout
+      <div className="layout">
+         <Head>
+            <title>JS Mastery Store</title>
+         </Head>
+         <header>
+            <Navbar/>
+         </header>
+         <main className="main-container">
+            EMPTY
+         </main>
+         <footer>
+            <Footer/>
+         </footer>
       </div>
    )
 }
