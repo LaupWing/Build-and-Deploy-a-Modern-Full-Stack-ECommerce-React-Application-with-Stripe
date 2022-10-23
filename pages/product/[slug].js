8e15961ee434d23f05fe317bef5f964b1a88e2d0
LaupWing <loc_strict@hotmail.com>
2022-10-23 10:37:24 +0200

    get static paths

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { urlFor } from '../../lib/client'
+import { client, urlFor } from '../../lib/client'
 
 const Product = ({
    product:{
@@ -24,6 +24,25 @@ const Product = ({
          </div>
       </div>
    )
+}
+
+export const getStaticPaths = async ({})=>{
+   const query = `*[_type == "product"] {
+      slug {
+         current
+      }
+   }`
+   const products = await client.fetch(query)
+   const paths = products.map(product=>({
+      params:{
+         slug: product.slug.current
+      }
+   }))
+
+   return {
+      paths,
+      fallback: "blocking"
+   }
 }
 
 export const getStaticProps = async ({
