b6c7e9451347979a03fe6975e5d034c8320e4947
LaupWing <loc_strict@hotmail.com>
2022-10-24 17:42:50 +0200

    lean and mean

--- components/Cart/index.js
+++ components/Cart/index.js
@@ -1,7 +1,24 @@
+import EmptyCart from "./_Cart_Empty";
+import CartProduct from "./_Cart_Product";
+
 const Cart = () => {
    return (
-      <div>
-         
+      <div className="cart-wrapper" ref={cartRef}>
+         <div className="cart-container">
+            <button
+               type="button"
+               className="cart-heading"
+               onClick={()=>{setShowCart(false)}}
+            >
+               <AiOutlineLeft/>
+               <span className="heading">Your Cart</span>
+               <span className="cart-num-items">({totalQuantities} items)</span>
+            </button>
+            {cartItems.length < 1 && <EmptyCart/>}
+            <div className="product-container">
+               {cartItems.length >= 1 && cartItems.map(item=> <CartProduct product={item}/>)}
+            </div>
+         </div>
       </div>
    )
 }
