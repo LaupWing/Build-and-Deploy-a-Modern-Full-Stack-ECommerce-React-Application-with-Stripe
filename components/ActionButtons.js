2fb18448097f8c640c52af03a442105e5c5de0e1
LaupWing <loc_strict@hotmail.com>
2022-10-24 09:56:34 +0200

    udpate

--- components/ActionButtons.js
+++ components/ActionButtons.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useStateContext } from '../context/StateContext'
 
 const ActionButtons = () => {
+   const {onAdd} = useStateContext()
+   
    return (
       <div className="buttons">
          <button
             className="add-to-cart"
             type="button"
+            onClick={onAdd}
          >
             Add to Cart
          </button>
