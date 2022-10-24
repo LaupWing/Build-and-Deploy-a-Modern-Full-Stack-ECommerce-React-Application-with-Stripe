c1fae730cfbfe1aaa2512bf9e81386c46e86dc54
LaupWing <loc_strict@hotmail.com>
2022-10-24 09:46:08 +0200

    onadd

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,6 +10,29 @@ export const StateContext = ({children}) =>{
    const [totalQuantities, setTotalQuantities] = useState()
    const [quantity, setQuantity] = useState(1)
 
+   const onAdd = (product, quantity)=>{
+      const checkProductInCart = cartItems.find((item)=>item._id === product._id)
+      setTotalPrice((prevTotalPrice)=> prevTotalPrice + product.price * quantity)
+      setTotalQuantities((prevTotalQuantities)=> prevTotalQuantities + quantity)
+
+      if(checkProductInCart){
+
+         const updatedCartItems = cartItems.map(cartProduct => {
+            if(cartProduct._id === product._id){
+               return {
+                  ...cartProduct,
+                  quantity: cartProduct.quantity + quantity
+               }
+            }
+         })
+         setCartItems(updatedCartItems)
+      } else{
+         product.quantity = quantity
+         setCartItems([...cartItems, {...product}])
+      }
+      toast.success(`${quantity} ${product.name} added to the cart`)
+   }
+
    const increaseQuantity = () =>{
       setQuantity((prevQuantity)=> prevQuantity + 1)
    }
@@ -26,7 +49,8 @@ export const StateContext = ({children}) =>{
             totalQuantities,
             quantity,
             increaseQuantity,
-            decreaseQuantity
+            decreaseQuantity,
+            onAdd
          }}
       >
          {children}
