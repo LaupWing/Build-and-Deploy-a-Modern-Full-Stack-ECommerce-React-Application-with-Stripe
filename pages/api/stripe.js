a9f85ff7997dd2f00f3f6b70fa50b8414fefc3e8
LaupWing <loc_strict@hotmail.com>
2022-10-26 09:18:12 +0200

    stripe fixed

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,17 +1,34 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 export default async function handler(req, res) {
    if (req.method === 'POST') {
       try {
          // Create Checkout Sessions from body params.
          const session = await stripe.checkout.sessions.create({
-            line_items: [
-               {
-                  // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                  price: '{{PRICE_ID}}',
-                  quantity: 1,
-               },
-            ],
+            line_items: req.body.map(item=>{
+               const img = item.image[0].assest._ref
+               const newImage = img
+                  .replace("image-", "https://cdn.sanity.io/images/8u1vrmu6/production")
+                  .replace("-webp", "webp")
+
+               return {
+                  price_data: {
+                     currency: "eur",
+                     product_data: {
+                        name: item.name,
+                        images: [newImage]
+                     },
+                     unit_amount: item.price * 100
+                  },
+                  adjustable_quantity: {
+                     enabled: true,
+                     minimum: 1
+                  },
+                  quantity: item.quantity
+               }
+            }),
             mode: 'payment',
             submit_type: "pay",
             payment_method_types: ["card"],
@@ -27,7 +44,7 @@ export default async function handler(req, res) {
             success_url: `${req.headers.origin}/?success=true`,
             cancel_url: `${req.headers.origin}/?canceled=true`,
          });
-         res.redirect(303, session.url);
+         res.status(200).json(session)
       } catch (err) {
          res.status(err.statusCode || 500).json(err.message);
       }
